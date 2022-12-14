b32146251b1456b5611a48b34a169d0efdaca73f
Quentin <quentin.geerts@bstorm.be>
2022-12-14 12:01:03 +0100

    Système de connexion

--- controllers/connexionController.php
+++ controllers/connexionController.php
@@ -1,16 +1,34 @@
 <?php 
     $msg = "";
-    if(isset($_POST["nom"])){
-        if($_POST["nom"] == "admin"){
-            // connecté
-            $_SESSION["nom"] = "admin";
-            header("Location:?section=vip");
+    
+    if ( isset($_POST['username'], $_POST['password']) ) {
+
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+        
+        // Vérification que les champs ont bien été remplis
+        if ($username !== "" && $password !== "") {
+
+            if ($username === "admin" && $password === "admin") {
+
+                $msg = "Connexion réussie";
+                $_SESSION["nom"] = $username;
+
+                // Redirection après 2 secondes vers la page VIP
+                header('refresh:2;url=?section=vip');
+
+            }
+            else {
+                $msg  = "<div class='error'>";
+                $msg .= "Identifiant/mot de passe incorrect.";
+                $msg .= "</div>";
+            }
+
         }
         else {
-            // msg erreur
-            $msg="<p>Mot de passe incorrect</p>";
+            $msg = "Veuillez entrer un identifiant/mot de passe.";
         }
+
     }
 
     include("views/page/connexion.php");
-?>
--- views/page/connexion.php
+++ views/page/connexion.php
@@ -1,7 +1,13 @@
 <h1>Connexion</h1>
 <form action="#" method="post">
-    <label for="nom">Nom</label>
-    <input type="text" name="nom" id="nom">
+    
+    <label for="username">Identifiant :</label>
+    <input type="text" name="username" id="username">
+    
+    <label for="password">Mot de passe :</label>
+    <input type="password" name="password" id="password">
+    
+    
     <input type="submit" value="Connexion">
 </form>
 <?= $msg; ?> 
